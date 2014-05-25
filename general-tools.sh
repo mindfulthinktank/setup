1269dbd37276a8503a61a7342e0f84ca078b5fb1
Ibrahim El Far <ibrahim_elfar@hotmail.com>
2014-05-25 13:59:46 -0700

    Modified general tools, test tools, and install tools: 	 - general tools: support for naming parameters 	 - test tools: makes use of named parameters for readability 	 - install tools: has option for specifying output document

---
+++ general-tools.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+function supplied {
+    if [ "${!1}" == "" ]; then
+	return 1;
+    else
+	return 0;
+    fi
+}
+
+function parameters {
+    i=1
+    while [ $i -le $# ]
+    do
+	j=$((i+1))
+	k=$((i+2))
+	case ${!i} in
+	    required)
+		declare -g ${!j}=${!k}
+		((i+=3))
+		;;
+	    optional)
+		if [ "${!k}" == "optional" ]; then
+		    declare -g ${!j}=""
+		    ((i+=2))
+		else
+		    declare -g ${!j}=${!k}
+		    ((i+=3))
+		fi
+		;;
+	    *)
+		echo "error in parameters; expecting optional | required"
+		
+		;;
+	esac
+    done
+}
